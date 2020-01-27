9c228426c802f3a5b23b8a3b4e3f0fc3351ee417
raythurman2386 <raymondthurman5@gmail.com>
2020-01-26 21:14:48 -0500

    rendering jokes to the page

---
+++ client/src/components/Jokes.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Jokes = () => {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('http://localhost:3300/api/jokes')
+      .then(res => setJokes(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <div>
+      <h1>Jokes</h1>
+      {jokes && jokes.map(joke => <p>{joke.joke}</p>)}
+    </div>
+  )
+}
+
+export default Jokes
+
+function axiosWithAuth() {
+  const token = localStorage.getItem('token')
+
+  return axios.create({
+    headers: {
+      authorization: token,
+      'Content-Type': 'application/json'
+    }
+  })
+}
