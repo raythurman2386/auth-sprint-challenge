c3624e1ca4067061039e514707566b1d2e395541
raythurman2386 <raymondthurman5@gmail.com>
2020-01-26 21:14:15 -0500

    app component set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Jokes from './components/Jokes'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Jokes</h1>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <nav>
+      <Link to='/'>Sign Up</Link>
+      <Link to='/login'>Log In</Link>
+    </nav>
+    <Switch>
+      <Route exact path='/' component={Signup} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/jokes' component={Jokes} />
+    </Switch>
+  </div>
+)
 
 export default App
