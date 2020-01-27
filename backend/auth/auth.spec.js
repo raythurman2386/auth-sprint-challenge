80ba3d17a867b7e57d1be2f5c9b01c4e05fcac39
raythurman2386 <raymondthurman5@gmail.com>
2020-01-26 21:42:13 -0500

    added describe blocks to tests

--- backend/auth/auth.spec.js
+++ backend/auth/auth.spec.js
@@ -6,42 +6,46 @@ beforeEach(async () => {
   await db('users').truncate();
 })
 
-test("register route", async () => {
+describe("register tests", () => {
+  test("register route", async () => {
 
-  const newUser = {
-    "username": "test",
-    "password": "test"
-  }
+    const newUser = {
+      "username": "test",
+      "password": "test"
+    }
 
-  const res = await supertest(server).post('/api/auth/register').send(newUser)
+    const res = await supertest(server).post('/api/auth/register').send(newUser)
 
-  expect(res.status).toBe(201);
-  expect(res.type).toBe('application/json');
-  expect(res.body.id).toBe(1)
-  expect(res.body.username).toMatch(/test/i)
+    expect(res.status).toBe(201);
+    expect(res.type).toBe('application/json');
+    expect(res.body.id).toBe(1)
+    expect(res.body.username).toMatch(/test/i)
+  })
+
+  test("register fail", async () => {
+    const res = await supertest(server).post('/api/auth/register')
+
+    expect(res.status).toBe(500)
+  })
 })
 
-test("register fail", async () => {
-  const res = await supertest(server).post('/api/auth/register')
+describe("login tests", () => {
+  test("login", async () => {
+    const user = {
+      "username": "test",
+      "password": "test"
+    }
+    await supertest(server).post('/api/auth/register').send(user)
+    const res = await supertest(server).post('/api/auth/login').send(user)
 
-  expect(res.status).toBe(500)
-})
+    expect(res.status).toBe(200)
+    expect(res.body.message).toContain("Welcome")
+    expect(res.body.token).toBeDefined()
+  })
 
-test("login", async () => {
-  const user = {
-    "username": "test",
-    "password": "test"
-  }
-  await supertest(server).post('/api/auth/register').send(user)
-  const res = await supertest(server).post('/api/auth/login').send(user)
+  test("login fail", async () => {
+    const res = await supertest(server).post('/api/auth/login')
 
-  expect(res.status).toBe(200)
-  expect(res.body.message).toContain("Welcome")
-  expect(res.body.token).toBeDefined()
-})
-
-test("login fail", async () => {
-  const res = await supertest(server).post('/api/auth/login')
-
-  expect(res.status).toBe(500);
+    expect(res.status).toBe(500);
+  })
 })
--- backend/jokes/jokes.spec.js
+++ backend/jokes/jokes.spec.js
@@ -1,9 +1,16 @@
 const supertest = require('supertest');
 const server = require('../api/server');
 
-test("get jokes", async () => {
-  const res = await supertest(server).get('/api/jokes')
 
-  expect(res.status).toBe(401)
-  expect(res.type).toBe('application/json')
+describe("jokes tests", () => {
+  test("get jokes", async () => {
+    const res = await supertest(server).get('/api/jokes')
+
+    expect(res.status).toBe(401)
+  })
+  test("2nd jokes", async () => {
+    const res = await supertest(server).get('/api/jokes')
+
+    expect(res.type).toBe('application/json')
+  })
 })
